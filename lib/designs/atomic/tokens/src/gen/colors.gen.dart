a70eaa4667f9deb3c4ade9700503ede0c77c92c7
jlamilladouble <jose.lamilla@doublevpartners.com>
2025-09-07 23:53:31 -0500

    feat: add color and asset generation files for SkeletonCard and CardWithShadow components

---
+++ lib/designs/atomic/tokens/src/gen/colors.gen.dart
@@ -0,0 +1,106 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class StoycoColors {
+  StoycoColors._();
+
+  /// Color: #4639E7
+  static const Color blue = Color(0xFF4639E7);
+
+  /// Color: #80000000
+  static const Color cardBackgroundBlack50 = Color(0x80000000);
+
+  /// Color: #00000000
+  static const Color cardBorderGradientEnd = Color(0x00000000);
+
+  /// Color: #4639E7
+  static const Color cardBorderGradientMid = Color(0xFF4639E7);
+
+  /// Color: #924539E7
+  static const Color cardBorderGradientPurple57 = Color(0x924539E7);
+
+  /// Color: #B74539E7
+  static const Color cardBorderGradientPurple72 = Color(0xB74539E7);
+
+  /// Color: #FFFFFF
+  static const Color cardBorderGradientStart = Color(0xFFFFFFFF);
+
+  /// Color: #1AFFFFFF
+  static const Color cardBorderGradientWhite05 = Color(0x1AFFFFFF);
+
+  /// Color: #26FFFFFF
+  static const Color cardBorderGradientWhite15 = Color(0x26FFFFFF);
+
+  /// Color: #304D1FE6
+  static const Color cardShadowPurple19 = Color(0x304D1FE6);
+
+  /// Color: #664D1FE6
+  static const Color cardShadowPurple40 = Color(0x664D1FE6);
+
+  /// Color: #1C197F
+  static const Color darkBlue = Color(0xFF1C197F);
+
+  /// Color: #0F151A
+  static const Color deepCharcoal = Color(0xFF0F151A);
+
+  /// Color: #D81736
+  static const Color error = Color(0xFFD81736);
+
+  /// Color: #D9D9D9
+  static const Color grayText = Color(0xFFD9D9D9);
+
+  /// Color: #92929D
+  static const Color hint = Color(0xFF92929D);
+
+  /// Color: #D5D5E0
+  static const Color iconDefault = Color(0xFFD5D5E0);
+
+  /// Color: #0E1926
+  static const Color midnightBlue = Color(0xFF0E1926);
+
+  /// Color: #4639E7
+  static const Color navbarButtonColor = Color(0xFF4639E7);
+
+  /// Color: #2B71FF
+  static const Color principal = Color(0xFF2B71FF);
+
+  /// Color: #4236DD
+  static const Color royalIndigo = Color(0xFF4236DD);
+
+  /// Color: #4F1FE68F
+  static const Color shadowBlue = Color(0x4F1FE68F);
+
+  /// Color: #494949
+  static const Color shadowColor = Color(0xFF494949);
+
+  /// Color: #0B122C
+  static const Color skeletonBase = Color(0xFF0B122C);
+
+  /// Color: #142358
+  static const Color skeletonHighlight = Color(0xFF142358);
+
+  /// Color: #F8F9FA
+  static const Color softWhite = Color(0xFFF8F9FA);
+
+  /// Color: #F2F2FA
+  static const Color text = Color(0xFFF2F2FA);
+
+  /// Color: #00000000
+  static const Color transparent = Color(0x00000000);
+
+  /// Color: #FAFAFA
+  static const Color white = Color(0xFFFAFAFA);
+
+  /// Color: #FFFFFF
+  static const Color white2 = Color(0xFFFFFFFF);
+}
