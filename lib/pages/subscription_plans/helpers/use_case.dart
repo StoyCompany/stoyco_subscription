09fa72ddc51b57bac6313cb27101c84136466974
jlamilladouble <jose.lamilla@doublevpartners.com>
2025-09-11 14:32:38 -0500

    feat: implement custom error handling classes and logging utilities for improved error management

--- lib/pages/subscription_plans/helpers/use_case.dart
+++
@@ -1,8 +0,0 @@
-import 'package:either_dart/either.dart';
-import 'package:stoyco_subscription/pages/subscription_plans/helpers/error/failure.dart';
-
-abstract class UseCase<T, Params> {
-  Future<Either<Failure, T>> call(Params params);
-}
-
-class NoParams {}
