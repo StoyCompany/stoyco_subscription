97f50e1fab4ff2c2806856c0006e4c1e04028ec5
alx-u <vertel@uninorte.edu.co>
2025-10-27 16:36:31 -0500

    :sparkles: feat: add dialog container v2, gradient border and update expired subscription modal design and functionality

--- lib/designs/atomic/molecules/modals/expired_subscription_modal.dart
+++ lib/designs/atomic/molecules/modals/expired_subscription_modal.dart
@@ -2,71 +2,98 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stoyco_subscription/designs/atomic/atoms/buttons/button_gradient.dart';
+import 'package:stoyco_subscription/designs/atomic/molecules/modals/dialog_container_v2.dart';
 import 'package:stoyco_subscription/designs/atomic/tokens/src/gen/assets.gen.dart';
 import 'package:stoyco_subscription/designs/atomic/tokens/src/gen/colors.gen.dart';
 import 'package:stoyco_subscription/designs/atomic/tokens/src/gen/fonts.gen.dart';
 import 'package:stoyco_subscription/designs/responsive/screen_size.dart';
 
 class ExpiredSubscriptionModal extends StatelessWidget {
-  const ExpiredSubscriptionModal({super.key});
+  const ExpiredSubscriptionModal({
+    super.key,
+    required this.onTapCloseButton,
+    required this.onTapMainButton,
+  });
+
+  final VoidCallback onTapCloseButton;
+  final VoidCallback onTapMainButton;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(
-          StoycoScreenSize.radius(context, 24),
-        ),
-      ),
-      color: StoycoColors.deepCharcoal,
+    return DialogContainer(
+      backgroundColor: StoycoColors.deepCharcoal,
       padding: StoycoScreenSize.symmetric(context, vertical: 8, horizontal: 16),
-      child: Column(
-        spacing: StoycoScreenSize.height(context, 16),
-        children: <Widget>[
-          StoycoAssets.lib.assets.icons.closeWindow.svg(
-            package: 'stoyco_subscription',
-          ),
-          Text(
-            'Tienes suscripciones vencidas',
-            style: TextStyle(
-              fontFamily: FontFamilyToken.akkuratPro,
-              fontWeight: FontWeight.w700,
-              fontSize: StoycoScreenSize.fontSize(context, 20),
-              color: StoycoColors.text,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: StoycoScreenSize.height(context, 16),
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: onTapCloseButton,
+                  child: StoycoAssets.lib.assets.icons.closeWindow.svg(
+                    package: 'stoyco_subscription',
+                    height: StoycoScreenSize.height(context, 24),
+                    width: StoycoScreenSize.width(context, 24),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Text(
-            'No pudimos procesar el pago con tu tarjeta. Actualiza tus datos para seguir disfrutando de beneficios exclusivos.',
-            style: GoogleFonts.inter(
-              textStyle: TextStyle(
-                fontSize: StoycoScreenSize.fontSize(context, 16),
+            Text(
+              'Tienes suscripciones vencidas',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: FontFamilyToken.akkuratPro,
+                fontWeight: FontWeight.w700,
+                fontSize: StoycoScreenSize.fontSize(context, 20),
                 color: StoycoColors.text,
-                fontWeight: FontWeight.w400,
               ),
             ),
-          ),
-          ButtonGradient(
-            child: Padding(
-              padding: StoycoScreenSize.symmetric(
-                context,
-                vertical: 12,
-                horizontal: 24,
+            Text(
+              'No pudimos procesar el pago con tu tarjeta. Actualiza tus datos para seguir disfrutando de beneficios exclusivos.',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.inter(
+                textStyle: TextStyle(
+                  fontSize: StoycoScreenSize.fontSize(context, 16),
+                  color: StoycoColors.text,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
-              child: Text(
-                'Renovar',
-                style: GoogleFonts.montserrat(
-                  textStyle: TextStyle(
-                    fontSize: StoycoScreenSize.fontSize(context, 16),
-                    color: StoycoColors.text,
-                    fontWeight: FontWeight.w700,
+            ),
+            ButtonGradient(
+              onPressed: onTapMainButton,
+              width: double.infinity,
+              backgroundGradientColor: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[StoycoColors.darkBlue, StoycoColors.blue],
+              ),
+              borderRadius: StoycoScreenSize.radius(context, 16),
+              child: Padding(
+                padding: StoycoScreenSize.symmetric(
+                  context,
+                  vertical: 12,
+                  horizontal: 24,
+                ),
+                child: Text(
+                  'Renovar ahora',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      fontSize: StoycoScreenSize.fontSize(context, 16),
+                      color: StoycoColors.text,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Gap(StoycoScreenSize.height(context, 16)),
-        ],
-      ),
+            Gap(StoycoScreenSize.height(context, 16)),
+          ],
+        ),
+      ],
     );
   }
 }
