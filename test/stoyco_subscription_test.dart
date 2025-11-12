079ee222758a54d6294b7d7cbb184227738f4e37
JSRamirezDEVDVP <juan.ramirez@doublevpartners.com>
2025-11-12 09:16:50 -0500

    chore: remove obsolete stoyco_subscription_test.dart file

--- test/stoyco_subscription_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('stoyco_subscription package tests', () {
-    // Los tests reales están en active_subscription_service_test.dart
-    expect(true, isTrue);
-  });
-}
